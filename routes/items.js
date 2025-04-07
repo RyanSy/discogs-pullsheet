38fd40933a1750a8e1b02db2b639aba7aa3cce07
Ryan Sy <ryanbsy@gmail.com>
2025-04-07 16:13:31 -0400

    improved error handling + deleted dotenv package + separated discogs

--- routes/items.js
+++ routes/items.js
@@ -1,134 +1,18 @@
 var express = require('express');
 var router = express.Router();
-var moment = require('moment');
-var start_date = moment().subtract(7, 'days').format();
-const Discogs = require('disconnect').Client;
-const paypal = require('../api/paypal');
+const discogs = require('../lib/discogs');
+const paypal = require('../lib/paypal');
 
 // render items view with list of discogs orders with 'payment received' status
-router.get('/', function(req, res) {
+router.get('/', function(req, res, next) {
   const accessData = req.session.accessData;
-
-  // get Discogs orders
-  function getDiscogsOrders(access_data, status, paypal_transactions) {
-    const mp = new Discogs(access_data).marketplace();
-
-    return mp.getOrders({ status: status, created_after: start_date })
-      .then(async function (data) {
-        var orders = data.orders;
-        var ordersArray = [];
-        for (var i = 0; i < orders.length; i++) {          
-          if (moment(orders[i].created) > moment(start_date)) {
-            var order = {};
-            var order_id = orders[i].id;
-            var paypal_data = {};
-            for (var j = 0; j < paypal_transactions.length; j++) {
-              if (order_id == paypal_transactions[j].invoice_id) {
-                paypal_data = paypal_transactions[j];
-              }
-            }
-            order.id = order_id;
-            order.date = orders[i].created;
-            order.created = moment(orders[i].created).format('lll');
-            order.updated = moment(orders[i].last_activity).format('lll');
-            order.buyer = orders[i].buyer.username;
-            order.status = orders[i].status;
-            order.total = orders[i].total.value;
-            order.additional_instructions = orders[i].additional_instructions;
-            order.shipping_method = orders[i].shipping.method;
-            order.shipping_amount = orders[i].shipping.value;
-            order.shipping_address = orders[i].shipping_address;
-            order.paypal_data = paypal_data;
-            order.messages = await getDiscogsOrderMessages(accessData, order_id);
-            order.items = [];
-            for (var j = 0; j < orders[i].items.length; j++) {
-              var item = {};
-              item.item_location = orders[i].items[j].item_location;
-              item.description = orders[i].items[j].release.description;
-              item.price = orders[i].items[j].price.value;
-              item.thumbnail = orders[i].items[j].release.thumbnail;
-              order.items.push(item);
-            }
-            ordersArray.push(order);
-          }
-        }
-        return ordersArray;
-      })
-      .catch(function(error) {
-        console.log(`Error getting Discogs ${status} orders.`);
-        catchError(error);
-        res.send('Server error.');
-      });
-  }
-
-  // get Discogs order messages
-  function getDiscogsOrderMessages(access_data, order_id) {
-    const mp = new Discogs(access_data).marketplace();
-
-    return mp.getOrderMessages(order_id)
-      .then(function(data) {
-        var messages = [];
-        for (var i = 0; i < data.messages.length; i++) {
-          if (data.messages[i].type === 'message') {
-            var messageObj = {};
-            messageObj.from = data.messages[i].from.username;
-            messageObj.message = data.messages[i].message;
-            messageObj.timestamp = moment(data.messages[i].timestamp).format('MMM D, YYYY h:mma');
-            messages.push(messageObj);
-          }
-        }
-        return messages;
-      })
-      .catch(function(error) {
-        console.log('Error getting Discogs order messages.');
-        catchError(error);
-        res.send('Server error.');
-      });
-  }
-
-  // get Discogs username
-  function getDiscogsUsername(access_data) {
-    const dis = new Discogs(access_data);
-
-    return dis.getIdentity()
-      .then(function(data) {
-        var username = data.username;
-        return username;
-      })
-      .catch(function(error) {
-        console.log('Error getting Discogs username.');
-        catchError(error);
-        res.send('Server error.');
-      });
-  }
-
-  // error handler
-  function catchError(error) {
-    if (error.response) {
-      // The request was made and the server responded with a status code
-      // that falls out of the range of 2xx
-      console.log(error.response.data);
-      console.log(error.response.status);
-      //  console.log(error.response.headers);
-    } else if (error.request) {
-      // The request was made but no response was received
-      // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-      // http.ClientRequest in node.js
-      console.log(error.request);
-    } else {
-      // Something happened in setting up the request that triggered an Error
-      console.log('Error', error.message);
-      console.log(error);
-    }
-    console.log(error.config);
-  }
-
+  
   // Initialize...
   (async function() {
     const paypalAccessToken = await paypal.getAccessToken();
     const paypalTransactions = await paypal.getTransactions(paypalAccessToken);
-    const paymentReceivedOrders = await getDiscogsOrders(accessData, 'Payment Received', paypalTransactions);
-    const username = await getDiscogsUsername(accessData);
+    const paymentReceivedOrders = await discogs.getOrders(accessData, 'Payment Received', paypalTransactions, next);
+    const username = await discogs.getIdentity(accessData, next);
     let message;
     paymentReceivedOrders.length === 0 ? message = 'No new orders found.' : message = null;
     const responseObj = {
@@ -139,6 +23,6 @@ router.get('/', function(req, res) {
 
     res.render('items', responseObj);
   })();
-}); // end items route
+});
 
 module.exports = router;
