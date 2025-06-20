c1733896693f6a446ddcfb23a211874152fd934f
ryansy <ryanbsy@gmail.com>
2025-06-20 00:48:54 -0400

    typos

--- routes/authorize.js
+++ routes/authorize.js
@@ -12,17 +12,17 @@ router.get('/', function(req, res, next) {
 	oAuth.getRequestToken(
 		process.env.DISCOGS_CONSUMER_KEY,
 		process.env.DISCOGS_CONSUMER_SECRET,
-		process.env.HOST + 'callback',
+		process.env.HOST + '/callback',
 		function(err, requestData){
 			if (err) {
 				console.log(err);
 				res.send("Error requesting oAuth token.");
-			}
-
-			// Persist "requestData" here so that the callback handler can
-			// access it later after returning from the authorize url
-			req.session.requestData = requestData;
-			res.redirect(requestData.authorizeUrl);
+			} else {
+				// Persist "requestData" here so that the callback handler can
+				// access it later after returning from the authorize url
+				req.session.requestData = requestData;
+				res.redirect(requestData.authorizeUrl);
+			}	
 		}
 	);
 });
